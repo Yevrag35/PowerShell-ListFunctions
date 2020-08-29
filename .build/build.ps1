01b999e84022c77b60e0b41f0cdb56bee7850465
Mike Garvey <mike.garvey@yevrag35.com>
2020-08-29 11:38:47 -0400

    Reorganize Folders

---
+++ .build/build.ps1
@@ -0,0 +1,15 @@
+﻿$builder = New-Object -TypeName "System.Text.StringBuilder"
+
+foreach ($priv in $(Get-ChildItem -Path "$PSScriptRoot\..\src\private" -Filter *.ps1 -Exclude "ComparerBuilder.ps1" -Recurse))
+{
+    [void] $builder.AppendLine((Get-Content -Path $priv.FullName -Raw))
+    [void] $builder.AppendLine()
+}
+
+foreach ($pub in Get-ChildItem -Path "$PSScriptRoot\..\src\public" -Filter *.ps1)
+{
+    [void] $builder.AppendLine((Get-Content -Path $pub.FullName -Raw))
+    [void] $builder.AppendLine()
+}
+
+Set-Content -Path "$PSScriptRoot\..\src\ListFunctions.psm1" -Value $builder.ToString() -Force
