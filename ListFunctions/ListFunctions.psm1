1bc3dce337365082bc74658ba4397910c907a4ad
Mike Garvey <mike.garvey@yevrag35.com>
2024-01-07 16:33:48 -0500

    Fix module and manifest
    Ready for publishing

--- ListFunctions/ListFunctions.psm1
+++ ListFunctions/ListFunctions.psm1
@@ -1,1 +1,18 @@
-﻿
+﻿switch ($PSVersionTable.PSVersion.Major) {
+    5 { $script:dllPath = "$PSScriptRoot\Desk\ListFunctions.NETFramework.dll" }
+    7 { $script:dllPath = "$PSScriptRoot\Core\ListFunctions.Next.dll" }
+    default { throw "Incompatible PowerShell Version" }
+}
+
+$script:assFolder = Split-Path -Path $script:dllPath -Parent
+$script:loadThese = @(
+    'MG.Collections.dll',
+    'ListFunctions.Engine.dll'
+)
+
+foreach ($script:assName in $script:loadThese) {
+
+    Import-Module "$($script:assFolder)\$($script:assName)"
+}
+
+Import-Module $script:dllPath -ErrorAction Stop
