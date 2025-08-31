ee79386f7c6ac6b50221f9580d90a0fb69fda23b
Mike Garvey <mike.garvey@yevrag35.com>
2025-08-31 13:34:05 -0400

    Add StructLayout and Enumerator for SingleValueReadOnlySet
    - Introduced a new `using` directive for `System.Runtime.InteropServices`.
    - Added a `StructLayout` attribute to `SingleValueReadOnlySet<T>` for sequential memory layout.
    - Removed the `_isNotEmpty` field; modified `IsEmpty` to check if `_value` is null.
    - Updated the `Contains` method to return false if the set is empty or the item is null.
    - Modified `IsProperSubsetOf`, `IsSubsetOf`, `IsSupersetOf`, and `SetEquals` methods to use `AsValueEnumerable()` for improved performance.
    - Changed `GetEnumerator` to return a custom `Enumerator` struct instead of yielding the value directly.
    - Defined a new `Enumerator` struct implementing `IEnumerator<T>` for iterating over the single value in the set.

--- src/engine/ListFunctions.Engine/Internal/SingleValueReadOnlySet.cs
+++ src/engine/ListFunctions.Engine/Internal/SingleValueReadOnlySet.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using ZLinq;
 
 namespace ListFunctions.Internal
@@ -18,14 +19,14 @@ namespace ListFunctions.Internal
         }
     }
 
+    [StructLayout(LayoutKind.Sequential)]
     internal readonly struct SingleValueReadOnlySet<T> : IReadOnlySet<T> where T : notnull
     {
         readonly IEqualityComparer<T> _equality;
-        readonly bool _isNotEmpty;
         readonly T _value;
 
         public int Count => 1;
-        internal bool IsEmpty => !_isNotEmpty;
+        internal bool IsEmpty => _value is null;
         internal T Value => _value;
 
         public SingleValueReadOnlySet(T value, IEqualityComparer<T>? equalityComparer)
@@ -37,7 +38,6 @@ namespace ListFunctions.Internal
 
             _equality = ResolveComparer(equalityComparer);
             _value = value;
-            _isNotEmpty = true;
         }
 
         private static IEqualityComparer<T> ResolveComparer(IEqualityComparer<T>? supplied)
@@ -45,7 +45,7 @@ namespace ListFunctions.Internal
             if (supplied is null)
             {
                 supplied = typeof(string).Equals(typeof(T))
-                    ? (IEqualityComparer<T>)StringComparer.InvariantCultureIgnoreCase
+                    ? (IEqualityComparer<T>)StringComparer.OrdinalIgnoreCase
                     : EqualityComparer<T>.Default;
             }
 
@@ -54,12 +54,7 @@ namespace ListFunctions.Internal
 
         public bool Contains(T item)
         {
-            if (this.IsEmpty || item is null)
-            {
-                return false;
-            }
-
-            return _equality.Equals(_value, item);
+            return !this.IsEmpty && _equality.Equals(_value, item);
         }
 
         public bool IsProperSubsetOf(IEnumerable<T> other)
@@ -67,18 +62,14 @@ namespace ListFunctions.Internal
             Guard.NotNull(other, nameof(other));
             if (this.IsEmpty)
             {
-                return other.Any();
+                return other.AsValueEnumerable().Any();
             }
 
+            var enumerator = other.AsValueEnumerable().GetEnumerator();
             DoubleBool dub = DoubleBool.InitializeNew();
-            foreach (T item in other)
+            while (!dub && enumerator.MoveNext())
             {
-                if (dub)
-                {
-                    return true;
-                }
-
-                if (_equality.Equals(_value, item))
+                if (_equality.Equals(_value, enumerator.Current))
                 {
                     dub.Bool1 = true;
                 }
@@ -99,7 +90,7 @@ namespace ListFunctions.Internal
                 return false;
             }
 
-            return !other.Any();
+            return !other.AsValueEnumerable().Any();
         }
 
         public bool IsSubsetOf(IEnumerable<T> other)
@@ -110,9 +101,9 @@ namespace ListFunctions.Internal
                 return true;
             }
 
-            foreach (T item in other)
+            foreach (T item in other.AsValueEnumerable())
             {
-                if (!(item is null) && _equality.Equals(_value, item))
+                if (_equality.Equals(_value, item))
                 {
                     return true;
                 }
@@ -126,12 +117,12 @@ namespace ListFunctions.Internal
             Guard.NotNull(other, nameof(other));
             if (this.IsEmpty)
             {
-                return !other.Any();
+                return !other.AsValueEnumerable().Any();
             }
 
-            foreach (T item in other)
+            foreach (T item in other.AsValueEnumerable())
             {
-                if (item is null || !_equality.Equals(_value, item))
+                if (!_equality.Equals(_value, item))
                 {
                     return false;
                 }
@@ -148,9 +139,9 @@ namespace ListFunctions.Internal
                 return false;
             }
 
-            foreach (T item in other)
+            foreach (T item in other.AsValueEnumerable())
             {
-                if (!(item is null) && _equality.Equals(_value, item))
+                if (_equality.Equals(_value, item))
                 {
                     return true;
                 }
@@ -164,7 +155,7 @@ namespace ListFunctions.Internal
             Guard.NotNull(other, nameof(other));
             if (this.IsEmpty)
             {
-                return !other.Any();
+                return !other.AsValueEnumerable().Any();
             }
 
             if (other.TryGetCount(out int count) && count != 1)
@@ -172,28 +163,53 @@ namespace ListFunctions.Internal
                 return false;
             }
 
-            foreach (T item in other)
-            {
-                count++;
-                if (count != 1 || item is null || !_equality.Equals(_value, item))
-                {
-                    return false;
-                }
-            }
-
-            return count == 1;
+            return other.AsValueEnumerable().SequenceEqual(this, _equality);
         }
 
-        public IEnumerator<T> GetEnumerator()
+        public Enumerator GetEnumerator()
         {
-            if (_isNotEmpty)
-            {
-                yield return _value;
-            }
+            return new(_value);
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            readonly T _value;
+            int _index;
+            public readonly T Current => _value;
+            readonly object? IEnumerator.Current => this.Current;
+
+            internal Enumerator(T value)
+            {
+                _value = value;
+                _index = -1;
+            }
+
+            public bool MoveNext()
+            {
+                if (_index == -1)
+                {
+                    _index = 0;
+                    return true;
+                }
+
+                _index = 1;
+                return false;
+            }
+            public void Reset()
+            {
+                _index = -1;
+            }
+            public readonly void Dispose()
+            {
+            }
         }
     }
 }
